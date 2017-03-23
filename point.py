dd85c3b7a2cfb3b030de6bf343f2936ec9171279
Teagan Gilbert <tjgilber@asu.edu>
2017-03-23 12:46:12 -0700

    Add files via upload

--- point.py
+++ point.py
@@ -2,10 +2,45 @@ import utils
 
 
 class Point(object):
+
     def __init__(self, x, y, mark={}):
         self.x = x
         self.y = y
         self.mark = mark
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return self.x != other.x and self.y != other.y
+
+    def __repr__(self):
+        #return """{}, {}""".format(self.x, self.y)
+        return "{0.__class__.__name__}(x={0.x}, y={0.y})".format(self)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __abs__(self):
+        return Point(abs(self.x), abs(self.y))
+
+
+class PointPattern(object):
+
+    def __init__(self, x, y, mark={}):
+        self.x = x
+        self.y = y
+        self.mark = mark
+
+    def __eq__(self, other):
+        return Point(self.x == other.x, self.y == other.y)
+
+
+
+
 
     def test_coincident(self, point_two):
         point_one = (self.x, self.y)
